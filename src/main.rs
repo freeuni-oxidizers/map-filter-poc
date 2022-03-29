9534a476cc6a63b5f7c0203db905b540a3810e50
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2022-03-29 20:52:05 +0400

    feat: example of user code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// load data(who loads it?)
+// map function(user defined closure/fn)
+// filter function(user defined closure/fn)
+// result comes back to master and gets printed
+
+// are we okay only supporting non-capturing functions directly
+
+// struct ListRdd<T> {
+//     values: Vec<T>
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // filter all odds
+    // double all
+    // should get multiples of 4
+    let rdd = ListRdd::new(values);
+    let result = rdd.filter(|x| x % 2 == 1).map(|x| 2 * x).collect();
+    println!("{}", result);
 }
